4537c798d5042cee2333ba308b47f4db2247dccd
Andrii Kanirovskyi <kanirovskyi@gmail.com>
2023-06-12 23:23:20 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,64 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesContainer = document.querySelector(".gallery");
+
+// функция создания карточки картинка в галерее
+function createImage(images) {
+    const imageItem = images.map(({ preview, original, description }) => `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+   `).join("");
+    
+    imagesContainer.insertAdjacentHTML("beforeend", imageItem);
+}
+
+//рендерится разметка
+createImage(galleryItems);
+
+imagesContainer.addEventListener("click", openModal);
+
+// функция открытия модалки
+function openModal(event) {
+  event.preventDefault();
+  const isClickable = event.target.classList.contains("gallery__image");
+  if (!isClickable) {
+    return
+  }
+
+  if (event.target) {
+    createModalImage();
+  }
+}
+// функция создания Большой картинка в модалке
+function createModalImage() {
+  const instance = basicLightbox.create(`
+  <img width="1400" height="900" src="${event.target.dataset.source}">
+  `);
+  instance.show();
+
+  document.addEventListener("keydown", closeModalImage);
+}
+// функция закрытия модалки и удаления слушателя событий
+function closeModalImage(event) {
+  const escKey = 'Escape';
+  if (event.code === escKey) {
+    deleteModalImage();
+
+    document.removeEventListener("keydown", closeModalImage);
+  }
+}
+// функция удаления Большой картинки в модалке
+function deleteModalImage() {
+  const instance = document.querySelector('.basicLightbox--visible');
+  instance.remove();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesContainer = document.querySelector(".gallery");
+
+// функция создания карточки картинка в галерее
+function createImage(images) {
+    const imageItem = images.map(({ preview, original, description }) => `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+   `).join("");
+    
+    imagesContainer.insertAdjacentHTML("beforeend", imageItem);
+}
+
+//рендерится разметка
+createImage(galleryItems);
+
+// Инициализация plugin lightbox и добавление параметров
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
